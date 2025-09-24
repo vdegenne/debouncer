f0934aca3c38893155c91db1d53c46418d9e0253
Valentin Degenne <vdegenne@gmail.com>
2025-09-24 17:44:05 +0200

    1.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,40 @@
 export class Debouncer<Args extends any[] = any[]> {
 	#timeout: number | undefined;
+	#resolve: ((value?: any) => void) | undefined;
 
 	constructor(
-		protected callback: (...args: Args) => void,
+		protected callback: (...args: Args) => void | Promise<any>,
 		protected timeoutMs = 100,
 	) {}
 
-	call(...args: Args) {
+	call(...args: Args): Promise<any> {
 		this.cancel();
-		this.#timeout = setTimeout(() => {
-			this.callback(...args);
-			this.#timeout = undefined;
-		}, this.timeoutMs);
+
+		return new Promise((resolve) => {
+			this.#resolve = resolve;
+
+			this.#timeout = setTimeout(async () => {
+				try {
+					const result = await this.callback(...args);
+					resolve(result);
+				} finally {
+					this.#timeout = undefined;
+					this.#resolve = undefined;
+				}
+			}, this.timeoutMs);
+		});
 	}
 
 	cancel() {
 		if (this.#timeout !== undefined) {
 			clearTimeout(this.#timeout);
 			this.#timeout = undefined;
+
+			// Reject or resolve the pending promise if needed
+			if (this.#resolve) {
+				this.#resolve(undefined);
+				this.#resolve = undefined;
+			}
 		}
 	}
 }
@@ -25,9 +42,9 @@ export class Debouncer<Args extends any[] = any[]> {
 const cache = new WeakMap<Function, Debouncer>();
 
 export function debounce<Args extends any[] = any[]>(
-	fn: (...args: Args) => void,
+	fn: (...args: Args) => void | Promise<any>,
 	delay = 100,
-): (...args: Args) => void {
+): (...args: Args) => Promise<any> {
 	let debouncer = cache.get(fn) as Debouncer<Args> | undefined;
 	if (!debouncer) {
 		debouncer = new Debouncer<Args>(fn, delay);
